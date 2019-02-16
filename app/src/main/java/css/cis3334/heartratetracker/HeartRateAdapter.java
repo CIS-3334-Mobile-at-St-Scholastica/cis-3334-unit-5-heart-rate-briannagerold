a15975927092495d404e3c0e30384957342ce0d7
briannagerold <bgerold@css.edu>
2019-02-16 12:58:30 -0600

    Assignment 5 - Heart Rate

--- app/src/main/java/css/cis3334/heartratetracker/HeartRateAdapter.java
+++ app/src/main/java/css/cis3334/heartratetracker/HeartRateAdapter.java
@@ -2,11 +2,14 @@ package css.cis3334.heartratetracker;
 
 import android.app.Activity;
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class HeartRateAdapter extends ArrayAdapter<HeartRate> {
 
@@ -17,11 +20,11 @@ public class HeartRateAdapter extends ArrayAdapter<HeartRate> {
      *
      * @param context The activity calling this adapter
      * @param rowLayout The xml file defining the layout for one item or row in the list
-     * @param dummyTV the ID for a TextView in the row layout. Not used, but needed by the parent object
+     * @param txtDummy the ID for a TextView in the row layout. Not used, but needed by the parent object
      * @param hrList The object holding the arraylist of hear rates
      */
-    public HeartRateAdapter(Context context, int rowLayout, int dummyTV, HeartRateList hrList) {
-        super(context, rowLayout, dummyTV, hrList.getList());
+    public HeartRateAdapter(Context context, int rowLayout, int txtDummy, HeartRateList hrList) {
+        super(context, rowLayout, txtDummy, hrList.getList());
         this.context = context;
         this.hrList = hrList;
     }
@@ -43,8 +46,33 @@ public class HeartRateAdapter extends ArrayAdapter<HeartRate> {
         //get the heart rate we are displaying
         HeartRate hr = hrList.getHeartRate(position);
 
-        TextView tvPulse=(TextView)view.findViewById(R.id.textViewPulse);
-        tvPulse.setText(hr.getPulse().toString());
+        TextView txtPulse=(TextView)view.findViewById(R.id.textViewPulse);
+        txtPulse.setText(hr.getPulse().toString());
+
+        TextView txtRange = view.findViewById(R.id.txtRange);
+        txtRange.setText(hr.getRangeName());
+
+        if(hr.getRangeName().equals("Resting")){
+            txtRange.setTextColor(ContextCompat.getColor(context, R.color.Resting));
+        }
+        else if(hr.getRangeName().equals("Moderate")){
+            txtRange.setTextColor(ContextCompat.getColor(context, R.color.Moderate));
+        }
+        else if(hr.getRangeName().equals("Endurance")){
+            txtRange.setTextColor(ContextCompat.getColor(context, R.color.Endurance));
+        }
+        else if(hr.getRangeName().equals("Aerobic")){
+            txtRange.setTextColor(ContextCompat.getColor(context, R.color.Aerobic));
+        }
+        else if(hr.getRangeName().equals("Anaerobic")){
+            txtRange.setTextColor(ContextCompat.getColor(context, R.color.Anaerobic));
+        }
+        else if(hr.getRangeName().equals("Red zone")){
+            txtRange.setTextColor(ContextCompat.getColor(context, R.color.RedZone));
+        }
+
+        TextView txtDescription = view.findViewById(R.id.txtDescription);
+        txtDescription.setText(hr.getRangeDescrtiption());
 
         return(view);
     }
